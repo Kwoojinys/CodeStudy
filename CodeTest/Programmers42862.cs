7b4bf42aa2824fbb8848c8b01047a654e9252111
Kwoojinys <aleldna54@gmail.com>
2024-11-05 18:08:10 +0900

    1. 문제 풀이 추가
    1. 문제 풀이 추가

---
+++ CodeTest/Programmers42862.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeTest
+{
+    // 프로그래머스 Lv.1 탐욕법 - 체육복
+    public class Solution42862
+    {
+        public int solution(int n, int[] lost, int[] reserve)
+        {
+            int answer = 0;
+
+            List<int> students = new List<int>();
+            answer = n - lost.Length;
+            Array.Sort(reserve, (x, y) => x > y ? 1 : -1);
+            Array.Sort(lost, (x, y) => x > y ? 1 : -1);
+            for (int i = 0; i < reserve.Length; i++)
+            {
+                var lostIndex0 = Array.IndexOf(lost, reserve[i]);
+                if (Array.IndexOf(lost, reserve[i]) >= 0)
+                {
+                    lost[lostIndex0] = -1;
+                    reserve[i] = -1;
+                    answer++;
+                    continue;
+                }
+
+                var lostIndex2 = Array.IndexOf(lost, reserve[i] - 1);
+                if (lostIndex2 >= 0 && Array.IndexOf(reserve, reserve[i] - 1) < 0)
+                {
+                    lost[lostIndex2] = -1;
+                    reserve[i] = -1;
+                    answer++;
+                    continue;
+                }
+
+                var lostIndex = Array.IndexOf(lost, reserve[i] + 1);
+                if (lostIndex >= 0 && Array.IndexOf(reserve, reserve[i] + 1) < 0)
+                {
+                    lost[lostIndex] = -1;
+                    reserve[i] = -1;
+                    answer++;
+                }
+
+            }
+
+            return answer;
+        }
+    }
+}
---
+++ CodeTest/Programmers81301.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeTest
+{
+    // 프로그래머스 Lv.1 2021 카카오 채용연계형 인턴십 - 숫자 문자열과 영단어
+    public class Solution81301
+    {
+        public int solution(string s)
+        {
+            int answer = 0;
+
+            string[] replaceArray = new string[] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+            for (int i = 0; i < replaceArray.Length; i++)
+            {
+                s = s.Replace(replaceArray[i], i.ToString());
+            }
+
+            answer = int.Parse(s);
+
+            return answer;
+        }
+    }
+}
