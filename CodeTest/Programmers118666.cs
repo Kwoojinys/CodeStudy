c0a6f1d7bfdaca0ceb4f35e9695bc4f8d8b2e711
Kwoojinys <aleldna54@gmail.com>
2024-11-07 17:17:40 +0900

    1. 문제 풀이 추가..
    1. 문제 풀이 추가..

---
+++ CodeTest/Programmers118666.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeTest
+{
+    // 프로그래머스 Lv.1 2022 KAKAO TECH INTERNSHIP - 성격 유형 검사하기
+    public class Solution118666
+    {
+        public string solution(string[] survey, int[] choices)
+        {
+            string answer = "";
+
+            Dictionary<string, int> scores = new Dictionary<string, int>()
+            {
+                {"R",0},
+                {"T",0},
+                {"C",0},
+                {"F",0},
+                {"J",0},
+                {"M",0},
+                {"A",0},
+                {"N",0}
+            };
+
+            for (int i = 0; i < survey.Length; i++)
+            {
+                var val = survey[i];
+
+                var score = Math.Abs(choices[i] - 4);
+                var targetScore = val[0];
+                if (choices[i] - 4 > 0)
+                {
+                    targetScore = val[1];
+                }
+
+                scores[targetScore.ToString()] += score;
+            }
+
+            for (int i = 0; i < scores.Count; i += 2)
+            {
+                answer += scores.Values.ElementAt(i) >= scores.Values.ElementAt(i + 1) ? 
+                    scores.Keys.ElementAt(i) : scores.Keys.ElementAt(i + 1);
+            }
+
+
+            return answer;
+        }
+    }
+}
---
+++ CodeTest/Programmers138477.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeTest
+{
+    // 프로그래머스 Lv.1 연습문제 - 명예의 전당 (1)
+    public class Solution138477
+    {
+        public int[] solution(int k, int[] score)
+        {
+            int[] answer        = new int[] { };
+            int[] hallOfFame    = new int[] { };
+
+            Array.Resize<int>(ref answer, score.Length);
+            Array.Resize<int>(ref hallOfFame, k);
+
+            for (int i = 0; i < hallOfFame.Length; i++)
+            {
+                hallOfFame[i] = -1;
+            }
+
+            for (int y = 0; y < score.Length; y++)
+            {
+                var s = score[y];
+
+                for (int i = 0; i < hallOfFame.Length; i++)
+                {
+                    if (hallOfFame[i] < s)
+                    {
+                        hallOfFame[i] = s;
+                        break;
+                    }
+                }
+
+                Array.Sort(hallOfFame, (a, b) => a >= b ? 1 : -1);
+
+                answer[y] = int.MaxValue;
+                foreach (var hall in hallOfFame)
+                {
+                    if (hall >= 0)
+                    {
+                        if (answer[y] > hall)
+                        {
+                            answer[y] = hall;
+                        }
+                    }
+                }
+            }
+
+            return answer;
+        }
+    }
+}
